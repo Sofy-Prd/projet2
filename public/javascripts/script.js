a8080421393cf13dc37793f46ed6c76fa59b2a8f
Sofy-Prd <sofy@DESKTOP-4LRCARK.localdomain>
2020-01-13 16:24:09 +0100

    css vue login terminé

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+// let prenomAdherent = prenom;
+
+// document.getElementById("prenomAdherent").onclick = function() {
+//   document.querySelector(".prenomAdherent").classList.remove("dontDisplay");
+// };
+
